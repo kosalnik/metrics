5f3b6a27e81747c4ac87f88583599d8343c7e24b
Alexandr Kosygin <a.kosygin@webinar.ru>
2024-03-03 15:43:56 +0400

    iter5: Конфигурирование через переменные окружения

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"github.com/kosalnik/metrics/internal/config"
+	"os"
+	"strconv"
 )
 
 func parseFlags(c *config.AgentConfig) {
@@ -10,4 +12,21 @@ func parseFlags(c *config.AgentConfig) {
 	flag.Int64Var(&c.PoolInterval, "p", 2, "Pool interval (seconds)")
 	flag.Int64Var(&c.ReportInterval, "r", 10, "Report interval (seconds)")
 	flag.Parse()
+
+	var err error
+	if v := os.Getenv("ADDRESS"); v != "" {
+		c.CollectorAddress = v
+	}
+	if v := os.Getenv("REPORT_INTERVAL"); v != "" {
+		c.ReportInterval, err = strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			panic("REPORT_INTERVAL should be Int64, got: " + v)
+		}
+	}
+	if v := os.Getenv("POLL_INTERVAL"); v != "" {
+		c.PoolInterval, err = strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			panic("POLL_INTERVAL should be Int64, got: " + v)
+		}
+	}
 }
--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -3,9 +3,13 @@ package main
 import (
 	"flag"
 	"github.com/kosalnik/metrics/internal/config"
+	"os"
 )
 
 func parseFlags(c *config.ServerConfig) {
 	flag.StringVar(&c.Address, "a", ":8080", "server endpoint (ip:port)")
 	flag.Parse()
+	if v := os.Getenv("ADDRESS"); v != "" {
+		c.Address = v
+	}
 }
